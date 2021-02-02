427758cc628e4f57c3f985c22bc01c91d1c4ba9b
Samppa Kivelä <n0kisa00@students.oamk.fi>
2021-02-02 12:41:14 +0200

    Komponentit ja navigaatio lisätty

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import ContactUs from './ContactUs';
+import NotFound from './NotFound';
+import Footer from './fragments/Footer';
+import Header from './fragments/Header';
+import NavBar from './fragments/Navbar';
 
 function App() {
   return (
-    <p>text</p>
+    <>
+    <NavBar />
+    <Header />
+    <div className="container">
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} exact />
+        <Route path="/contactus" component={ContactUs} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+    <Footer />
+    </>
   );
 }
 
